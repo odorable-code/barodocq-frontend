68c4fcf5854955b4dce28d2d7a7884002619443b
shh3482 <shh3482@gmail.com>
2026-02-19 16:44:09 +0900

    예약현황 만들기 기본세팅 작업

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import Main from './Main';
 
+// 예약 현황 조회 불러오기
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import BookingDetail from "./pages/booking/BookingDetail";
+
 function App() {
   return (
-    <div className="App">
-      <Main/>
-    </div>
+    // <div className="App">
+    //   <Main/>
+    // </div>
+    <Router>
+      <Routes>
+        <Route path="/booking-detail" element={<BookingDetail />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/ReservationDetail.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./BookingDetail.css";
+
+function BookingDetail() {
+  return (
+    <div className="overlay">
+      <div className="popup">
+        <h2>예약 상세</h2>
+        <p>여기에 예약 정보를 넣을 수 있습니다.</p>
+      </div>
+    </div>
+  );
+}
+
+export default BookingDetail;
