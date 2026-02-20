5719983db57fab78cb89fca4a529d8d1026264d4
shh3482 <63386461+shh3482@users.noreply.github.com>
2026-02-20 11:53:17 +0900

    메인에 머지하면서 발견한 오류들 수정
    x랑 빈공간 클릭해도 안나가지는 버그 수정

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import ReservationDateSelect from "./pages/ReservationDateSelect";
 
 function App() {
-  const [showPopup, setShowPopup] = useState(false);
-
   return (
     <div>
-      <button onClick={() => setShowPopup(true)}>예약하기</button>
-      {showPopup && (
-        <ReservationDateSelect onClose={() => setShowPopup(false)} />
-      )}
+      <ReservationDateSelect />
     </div>
   );
 }
