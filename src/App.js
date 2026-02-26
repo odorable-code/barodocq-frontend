e5970841c803c2ed66179934a1b4e10e25234cbe
Kim-yeonhee <hiasis5@naver.com>
2026-02-26 20:07:54 +0900

    오류수정제발2

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@ import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Chat from "./Chat/Chat";
-<<<<<<< HEAD
 
-=======
->>>>>>> parent of 99bf8a1 (오류수정)
 import ChatList from "./Chat/ChatList";
 import Layout from "./components/Layout";
 import Main from "./Main";
@@ -21,11 +18,8 @@ import ReviewCreate from "./Review/ReviewCreate";
 import ReviewDetail from "./Review/ReviewDetail";
 import ReviewRevise from "./Review/ReviewRevise";
 import PharmacySearch from "./pages/PhamacySearch";
-<<<<<<< HEAD
 
 
-=======
->>>>>>> parent of 99bf8a1 (오류수정)
 
 // 관리자 페이지
 import AdminCustomers from "./adminComponents/AdminCustomers";
